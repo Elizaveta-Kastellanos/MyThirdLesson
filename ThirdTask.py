b5d500edba23ffaffd0131f620d4fbad51e02c7b
Elizaveta_Kastellanos <lizusiku28@mail.ru>
2022-08-21 23:34:14 +0300

    ready uhu

--- ThirdTask.py
+++ ThirdTask.py
@@ -11,7 +11,6 @@ for i in range(len(my_list_for_sum)):
         sum += my_list_for_sum[i]
 print(sum)
 
-
 #2.) Напишите программу, которая найдёт произведение пар чисел списка.
 #  Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 #  Пример: - [2, 3, 4, 5, 6] => [12, 15, 16];  - [2, 3, 5, 6] => [12, 15]
@@ -26,8 +25,6 @@ for i in range(len(my_list_for_mult)//2):
     else:
         new_list_for_mult.append(my_list_for_mult[i] * my_list_for_mult[len(my_list_for_mult)-i-1])
 print(new_list_for_mult)
-
-print('-------------------')
 
 #3.) Задайте список из вещественных чисел. Напишите программу, 
 # которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
@@ -47,25 +44,39 @@ print(round(max_num-min_num, 20))
 # 4.) Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 # Пример: - 45 -> 101101;  - 3 -> 11;  - 2 -> 10    
 
-num = int(input('Введите десятичное число: '))
-num2 = ''
-while num > 0:
-    num2 = num2 + str(num % 2) 
-    num //= 2
-print(num2)
+try:
+    num = int(input('Введите десятичное число: '))
+    num2 = ''
+    while num > 0:
+        num2 = str(num % 2) + num2
+        num //= 2
+    print(num2)
+except ValueError:
+    print('Упс! Что-то пошло не так, попробуй ввести число!')
 
 # 5.) Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 # Пример:  - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
-# [Негафибоначчи]
-# (https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%
-# D0%B3%D0%B0%D1%84%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8#:~:
-# text=%D0%92%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B
-# 5%2C%20%D1%87%D0%B8%D1%81%D0%BB%D0%B0%20%D0%BD%D0%B5%D0%B3%D0%B0%D1%84%D0%
-# B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8%20%E2%80%94%20%D0%BE%D1%82%D1%80%
-# D0%B8%D1%86%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%20%D0%B8%D0%BD%D0%B4%D0%B
-# 5%D0%BA%D1%81%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%8D%D0
-# %BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D1%8B%20%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%
-# D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20%D1%87
-# %D0%B8%D1%81%D0%B5%D0%BB%20%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8.)    
 
-
+try:
+    mas = []
+    f0, f1 = 1, 1
+    while True:
+       n = int(input('Введите число N: '))
+       if n > 2 and n < 20:
+        break
+    nn = -n
+    i = 1
+    f_1 = f1 - f0
+    while i >= nn:
+        f0, f1 = f1 - f0, f0
+        mas.append(f1)
+        i = i - 1
+    mas.reverse()
+    mas.append(1)
+    f0, f1 = 1, 1
+    for i in range(2, n):
+         f0, f1 = f1, f0 + f1
+         mas.append(f1)
+    print(*mas)
+except ValueError:
+    print('Упс! Что-то пошло не так, попробуй ввести число!')
